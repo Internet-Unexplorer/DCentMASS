4bb094048a88700966bcf1c5db042883b5a35c73
Girish <girishsourceus49@gmail.com>
2017-12-28 11:03:50 +0530

    Fixed and Updated

--- stylesAndFont.php
+++ stylesAndFont.php
@@ -1,41 +1,24 @@
 <!-- Font family -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans|Poppins|Roboto|Titillium+Web" rel="stylesheet">
-
-
-
-
 <!-- Bootstrap CSS 4! -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- MaterialDesign -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <!-- <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"> -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
 
 <!-- Custom CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
 <link rel="stylesheet" type="text/css" href="css/style.css">
-<link href="https://fonts.googleapis.com/css?family=Roboto">
 <!-- SASS output -->
 <link rel="stylesheet" type="text/css" href="css/style1.css">
 
 
-<<<<<<< HEAD
-=======
 
 
 
 
-
-
-
-
-
-
-
-
->>>>>>> 204f83fbafdc5b7785fd64f2f94130c001675453
 <!-- User CSS -->
 <link rel="stylesheet" type="text/css" href="css/nadeem.css">
 
@@ -46,7 +29,11 @@
 <link rel="stylesheet" type="text/css" href="css/sumaiya.css">
 
 <link rel="stylesheet" type="text/css" href="css/asgar.css">
+
+
+
+
+
+
+
 <link rel="stylesheet" type="text/css" href="css/girish.css">
-
-
-
