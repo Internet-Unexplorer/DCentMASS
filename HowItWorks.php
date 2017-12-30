508f6ada20d68807f2b9fb7c85f89710f83e54b2
Nadeem <nadeem566sourceus@gmail.com>
2017-12-30 09:54:58 +0530

    edited howitworks.php

--- HowItWorks.php
+++ HowItWorks.php
@@ -100,8 +100,9 @@ Date:        14/12/17
 </div>
 </div>
 <div class="row">
-<div class="col-md-12"><br/>
+<div class="col-md-12">
 <span class="main-heading">How It Works</span>
+<hr>
 <img src="images/workflow.png" class="wfimg img-fluid"><br><hr>
 
 <div class="container-fluid">
@@ -296,6 +297,42 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 
 
 </div>
+
+<div class="row">
+
+<div class="col-md-4">
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="images/advantage1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="images/advantage1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="images/advantage1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+
+</div>
+<hr>
 </div>       
 <!-- MAIN CONTENT ENDS -->
 	
